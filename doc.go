36968238b4bbb96e741ab99f4b074599d6f03614
Steve Miskovetz <steve.miskovetz@gmail.com>
2024-08-27 15:31:06 -0600

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,11 +1,10 @@
 package celestia_da_light_client
 
 /*
-celestia light client is based on the ibc-go 07-tendermint and 07-celestia light client.
-It has no dependencies on ibc-go. It uses json encoding instead of proto encoding due
-to the proto dependencies not using pulsar while tiablob does.
-Then encoding may change in the future, but the additional overhead should not be
-significant since only 1 client will be present.
-Pruning is TBD, we may only require the latest consensus state.
-The consensus state root also uses the data hash instead of app hash.
+celestia-da-light-client is a Celestia tracking light client for proving data inclusion.
+It is primarily based on the ibc-go 07-tendermint light client, but has no ibc-go dependencies. 
+It is similar to the 07-celestia light client whereas it uses the data hash for the
+consensus state root instead of the app hash and re-uses celestia-core's share/row proof validation.
+Unlike 07-celestia, it does not use 07-tendermint's client state and does not require ibc-go
+core modifications (as of this writing).
 */
